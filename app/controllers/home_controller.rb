5e74f29703737b63ea43dbff7aba1d7a09985428
Bob Walsh <bob.walsh@47hats.com>
2022-10-06 17:47:27 -0700

    return raw weather data for fixed address

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
+  require 'net/http'
   def index
+    url = "https://api.weatherstack.com/current?access_key=438b623c8e56f25eaee02d77471cfc76&query=New%20York"
+    uri = URI(url)
+    res = Net::HTTP.get_response(uri)
+    @data = JSON.parse(res.body)
+    puts JSON.pretty_generate(res.body)
   end
 end
