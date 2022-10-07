a389f965fb3ea0ab933d1bd69ca04012c1b670f1
Bob Walsh <bob.walsh@47hats.com>
2022-10-07 10:23:35 -0700

    wip

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,23 @@
 class HomeController < ApplicationController
   require 'net/http'
   def index
-    url = "https://api.weatherstack.com/current?access_key=438b623c8e56f25eaee02d77471cfc76&query=New%20York"
+    # url = "https://api.weatherstack.com/current?access_key=438b623c8e56f25eaee02d77471cfc76&query=New%20York"
+    url = "https://api.weatherstack.com/current?access_key=438b623c8e56f25eaee02d77471cfc76&units=f&query=Quebec&feelslike"
     uri = URI(url)
     res = Net::HTTP.get_response(uri)
     @data = JSON.parse(res.body)
-    puts JSON.pretty_generate(res.body)
+    # puts JSON.pretty_generate(res.body)
+
+    url2 = "https://api.weatherstack.com/historical?access_key=438b623c8e56f25eaee02d77471cfc76&units=f&query=Quebec&historical_date=2022-10-05"
+    uri2 = URI(url2)
+    res2 = Net::HTTP.get_response(uri2)
+    @data2 = JSON.parse(res2.body)
+
+    url3 = "https://api.weatherstack.com/forecast?access_key=438b623c8e56f25eaee02d77471cfc76&units=f&query=Quebec&"
+    uri3 = URI(url3)
+    res3 = Net::HTTP.get_response(uri3)
+    @data3 = JSON.parse(res3.body)
+    # raise
+    # puts JSON.pretty_generate(res3.body)
   end
 end
